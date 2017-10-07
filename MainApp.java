0da7b11aeb443ef83a365a0248ac0bb63f176fe6
AlexeyCherenko <32494816+AlexeyCherenko@users.noreply.github.com>
2017-10-07 19:32:20 +0300

    Add files via upload

--- MainApp.java
+++ MainApp.java
@@ -1,24 +1,27 @@
 package by.gsu.gomel;
 
+import java.util.ArrayList;
+
 public class MainApp {
-
     public static void main(final String[] args) {
-        final IceCream[] moroz = {new IceCream("Lakomka", 10, 14.5),
-                new IceCream("Pingvin", 3, 10.0), new IceCream("Ykki", 4, 11.1),
-                new IceCream("Soletto", 5, 11.0), new IceCream("Plombir", 3, 13.5),
-                new IceCream("Noon", 20, 0.6), new IceCream("9Pash", 5, 1.9),
-                new IceCream("Lil", 6, 1.11), new IceCream("Solo", 3, 1.14),
-                new IceCream("Ramz", 15, 0.11)};
-
-        float averageFat = 0;
-        int kolvoch = 0;
-        for (final IceCream i : moroz) {
-            System.out.format("Name/Shoco/Fatness:  %-9s/%-4d/%.1f%n", i.getName(), i.getShoco(),
-                    i.getFatness());
-            averageFat += i.getFatness();
-            kolvoch += i.getShoco();
+        final ArrayList<IceCream> icecream = new ArrayList<>();
+        icecream.add(new IceCream("Lakomka", 10, 14.5));
+        icecream.add(new IceCream("Pingvin", 3, 10.0));
+        icecream.add(new IceCream("Ykki", 4, 11.1));
+        icecream.add(new IceCream("Soletto", 5, 11.0));
+        icecream.add(new IceCream("Plombir", 3, 13.5));
+        icecream.add(new IceCream("Moon", 20, 0.6));
+        icecream.add(new IceCream("28Cent", 5, 1.9));
+        icecream.add(new IceCream("Lil", 6, 1.11));
+        icecream.add(new IceCream("322", 3, 1.14));
+        icecream.add(new IceCream("Rome", 15, 0.11));
+        float AllFat = 0;
+        int AllCh = 0;
+        for (int i = 0; i < 10; i++) {
+            AllFat += icecream.get(i).getFatness();
+            AllCh += icecream.get(i).getShoco();
         }
-        System.out.println("AvgFat = " + (averageFat / moroz.length));
-        System.out.println("Kolvo_ch = " + kolvoch);
+        System.out.println("AvgFat = " + (AllFat / icecream.size()));
+        System.out.println("Kolvo_ch = " + AllCh);
     }
 }
